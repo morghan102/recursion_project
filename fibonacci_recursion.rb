558f5fd582ac6e5dd818ad1a55a731110de8a263
Morghan <morghan102@gmail.com>
2020-01-02 10:17:36 -0500

    Made a mistake, added before saving

--- fibonacci_recursion.rb
+++ fibonacci_recursion.rb
@@ -1,8 +1,10 @@
 def fib (num)
-
+    arr = []
+    
+    arr << num
 end
 
 def fib_rec (num)
-    return 1 if num < 2
-    fib_rec((num -1) + (num - 2))
+    return num if num < 2
+    fib_rec(num - 1) + fib_rec(num - 2)
 end
