d353b8d989c1b80356a0b2194e0a81586ef7d938
Morghan <morghan102@gmail.com>
2020-01-10 16:50:07 -0500

    Finished merge_sort (hopefully)

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,16 +1,21 @@
 def merge_sort (arr)
-    longness = arr.length
-    left, right = []
-    if longness <= 1 
-     arr
-    else
-        left = arr[0..(longness-1)/2]
-        right = arr[(longness+1)/2..-1]
-
+  longness = arr.length
+  left = [] #!!!how to declare right in same line?
+  right = []
+  if longness <= 1 
+   arr
+  else
+      left = arr[0..((longness-1)/2)]
+      right = arr[((longness+1)/2)..-1]
+      merge(arr,left,right)
   end
-p left, right
 end
 
 def merge (arr, left, right)
-
+  if left.length > 1
+    merge_sort(??) #base case. send back to divide again in mrgsort
+  else
+    p left, right
+    #put it back together
+  end
 end
