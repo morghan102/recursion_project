d5fb66711eace5c0acdbdb18025085639b1123c7
Morghan <morghan102@gmail.com>
2020-01-10 20:43:00 -0500

    Stuck on merge function

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,3 +1,5 @@
+arr = [0,1,2,3,4,5,6]
+
 def merge_sort (arr)
   longness = arr.length
   left = [] #!!!how to declare right in same line?
@@ -5,17 +7,22 @@ def merge_sort (arr)
   if longness <= 1 
    arr
   else
-      left = arr[0..((longness-1)/2)]
-      right = arr[((longness+1)/2)..-1]
+      mid = longness/2
+      left = arr[0..(mid-1)]
+      right = arr[mid..-1]
       merge(arr,left,right)
   end
 end
 
 def merge (arr, left, right)
-  if left.length > 1
-    merge_sort(??) #base case. send back to divide again in mrgsort
+  finished_arr = []
+  if left.length < 1
+    p arr
+#base case. send back to divide again in mrgsort
   else
-    p left, right
-    #put it back together
+    if left[0] > right[0]
+      finished_arr << left, right
+    end
   end
 end
+merge_sort(arr)
