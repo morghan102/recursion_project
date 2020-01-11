089b510e7d2c88c8e4e0759d0b8b40f9c747e8e1
Morghan <morghan102@gmail.com>
2020-01-11 14:54:31 -0500

    Finished merge function, next is the recursion

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,4 +1,4 @@
-arr = [0,1,2,3,4,5,6]
+arr = [1,2,3,4,5,6]
 
 def merge_sort (arr)
   longness = arr.length
@@ -10,19 +10,23 @@ def merge_sort (arr)
       mid = longness/2
       left = arr[0..(mid-1)]
       right = arr[mid..-1]
-      merge(arr,left,right)
+      merge(left, right)
   end
 end
 
-def merge (arr, left, right)
+def merge (left, right)
   finished_arr = []
-  if left.length < 1
-    p arr
-#base case. send back to divide again in mrgsort
-  else
-    if left[0] > right[0]
-      finished_arr << left, right
-    end
+  a = left[0]
+  b = right[0]
+  while left.length > 0 && right.length > 0 do
+#base case. send back to divide again in mrgsort(?)
+  if a > b
+      finished_arr.push(left.shift)
+  elsif b > a
+    finished_arr.push(right.shift)
+  #if its greater, pop it out of the old array and push into new one
   end
+  end
+  p finished_arr
 end
 merge_sort(arr)
